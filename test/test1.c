df435f6bb0a43d7bb9b56a395a635090a7cc175c
Rainer Poisel <rainer.poisel@gmail.com>
2020-07-15 14:07:36 +0200

    enable all cppcheck checks

--- test/test1.c
+++ test/test1.c
@@ -28,9 +28,7 @@ int main(void)
 {
   UnityBegin(__FILE__);
 
-  // cppcheck-suppress [preprocessorErrorDirective]
   RUN_TEST(testAddBad);
-  // cppcheck-suppress [preprocessorErrorDirective]
   RUN_TEST(testAddGood);
 
   return UnityEnd();
